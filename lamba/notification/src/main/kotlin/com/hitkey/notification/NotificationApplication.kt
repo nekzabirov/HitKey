8c69fb6555700ee32372dce1b47f115436726bec
Nekbakht Zabirov <neka1dev@gmail.com>
2023-03-07 03:44:42 +0200

    Enable cloud discovery

--- lamba/notification/src/main/kotlin/com/hitkey/notification/NotificationApplication.kt
+++ lamba/notification/src/main/kotlin/com/hitkey/notification/NotificationApplication.kt
@@ -2,8 +2,10 @@ package com.hitkey.notification
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class NotificationApplication
 
 fun main(args: Array<String>) {
