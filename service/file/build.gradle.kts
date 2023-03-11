21e9e392401154fcf0de28f888db8c4c60f889d3
Nekbakht Zabirov <neka1dev@gmail.com>
2023-03-11 02:35:34 +0200

    Set user security

--- service/file/build.gradle.kts
+++ service/file/build.gradle.kts
@@ -8,4 +8,6 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     providedRuntime("io.jsonwebtoken:jjwt-impl:0.11.2")
     providedRuntime("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
+    implementation("org.springframework.boot:spring-boot-starter-security")
 }
---
+++ service/file/src/main/kotlin/com/hitkey/file/config/WebSecurityConfig.kt
@@ -0,0 +1,49 @@
+package com.hitkey.file.config
+
+import com.hitkey.file.component.AuthenticationManager
+import com.hitkey.file.component.SecurityContextRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+import reactor.core.publisher.Mono
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+class WebSecurityConfig {
+    @Autowired
+    private lateinit var authenticationManager: AuthenticationManager
+
+    @Autowired
+    private lateinit var securityContextRepository: SecurityContextRepository
+
+    @Bean
+    fun securityWebFilterChain(httpSecurity: ServerHttpSecurity): SecurityWebFilterChain = httpSecurity
+        .exceptionHandling()
+        .authenticationEntryPoint { swe, _ ->
+            Mono.fromRunnable {
+                swe.response.statusCode = HttpStatus.UNAUTHORIZED
+            }
+        }
+        .accessDeniedHandler { swe, _ ->
+            Mono.fromRunnable {
+                swe.response.statusCode = HttpStatus.FORBIDDEN
+            }
+        }
+        .and()
+        .csrf().disable()
+        .formLogin().disable()
+        .httpBasic().disable()
+        .authenticationManager(authenticationManager)
+        .securityContextRepository(securityContextRepository)
+        .authorizeExchange()
+        .pathMatchers("/app/*/info").permitAll()
+        .anyExchange().authenticated()
+        .and()
+        .build()
+}
